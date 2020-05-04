8435bfa309e5ffb04e90571fb8d1a8e1220c2477
YingMa1993 <yingma@umich.edu>
2020-05-03 22:20:29 -0400

    Update bubble_plot.R

--- figures/bubble_plot.R
+++ figures/bubble_plot.R
@@ -5,40 +5,31 @@
 ##-------------------------------------------------------------
 ## Create data frame for bubble plot
 ##-------------------------------------------------------------
+load("./plotdata_Figure4e.RData")
 
-load("./plotdata_Figure5e.RData")
 #### bubble plot
-Sig=plotdata[which(plotdata$Log>30),]
-BP = Sig[which(Sig$Category=="BIOLOGICAL PROCESS"),]
-OTH = Sig[which(Sig$Category=="OTHERS"),]
-MF =Sig[which(Sig$Category=="MOLECULAR FUNCTION"),]
-CC = Sig[which(Sig$Category=="CELLULAR COMPONENT"),]
-BP = BP[order(BP$Adj_Pval,decreasing = F),]
-OTH = OTH[order(OTH$Adj_Pval,decreasing = F),]
-MF = MF[order(MF$Adj_Pval,decreasing = F),]
-CC = CC[order(CC$Adj_Pval,decreasing = F),]
+#### order the data frame by category of the gene sets
+CateoryLevels = c("IMMUNOLOGIC SIGNATURES", "CHEMICAL AND GENETIC PERTURBATIONS", "GO BIOLOGICAL PROCESS",
+"GO MOLECULAR FUNCTION","GO CELLULAR COMPONENT","ONCOGENIC SIGNATURES","REACTOME","KEGG","PID","BIOCARTA")
+plotdata = plotdata[order(match(plotdata$Category, CateoryLevels)),]
 
-names1 = c(unique(BP$Term)[c(2,4,7,8)],
-unique(CC$Term)[c(1:2)],
-unique(OTH$Term)[c(1,2,4)],
-unique(MF$Term)[2])
-Sig = Sig[which(Sig$Term %in% names1),]
-Sig = Sig[!duplicated(Sig$Term),]
-unique(Sig$Term)
+#### select the biologically significant gene set in the bubbleplot to show
+SigSelectTem = c("GSE29618_PDC_VS_MDC_DN","GO_VASCULATURE_DEVELOPMENT","GO_BLOOD_VESSEL_MORPHOGENESIS","LIM_MAMMARY_STEM_CELL_UP",
+                "GO_ENDODERM_DEVELOPMENT","GO_ANCHORING_JUNCTION","ONDER_CDH1_TARGETS_2_DN","GO_EXTRACELLULAR_MATRIX","GO_ANGIOGENESIS",
+                "GO_CELL_ADHESION_MOLECULE_BINDING")
+Sig = plotdata[which(plotdata$Term %in% SigSelectTem),]
+#### you can modify your text label here .i.e. not capitalize the gene set term 
+Sig$Term = tolower(Sig$Term)
 
 ##-------------------------------------------------------------
 ## ggplot function for bubble plot
 ##-------------------------------------------------------------
 library(ggplot2)
 library("ggrepel")
-p5 = ggplot(plotdata, aes(x = IDNum, y = Log,color = Category))+
-geom_point(shape = 19,aes(fill = Category, size = Count,shape = 19),alpha=0.2)+
+p4 = ggplot(plotdata, aes(x = IDNum, y = Log,color = Category))+
+geom_point(shape = 19,aes(fill = Category, size = Count,shape = 19),alpha=0.8)+
 scale_radius()+
-labs(x = "", y = expression(paste(bold(-log[10])," ",bolditalic(p),bold("-value"))))+
-scale_size(name   = "Gene set size",
-breaks = c(3000,6000,9000),
-labels = c("3000","6000","9000"),
-range = c(2,10))+
+labs(x = "", y = expression(paste(bold(-log[10]),bold("("),bolditalic(p),bold("-value)"))))+
 theme(plot.margin = margin(1, 1, 1, 1, "cm"),
 axis.text.x=element_blank(),
 plot.title = element_text(lineheight=.8, face="bold"),
@@ -58,12 +49,12 @@ geom_hline(yintercept = 1.82, col = 'black',linetype = 2,size=2)+
 guides(color = guide_legend(order = 1,override.aes = list(alpha = 1,size=7)),
 size = guide_legend(order = 2,override.aes = list(alpha = 1,shape=21)),
 fill = FALSE)+ labs(size = "Gene set size")+
-scale_color_manual(values=c("salmon","gold2","#42d4f4","#3cb44b"))+
-scale_fill_manual(values = c("salmon","gold2","#42d4f4","#3cb44b"))+
+scale_color_manual(values=c("salmon","gold2","#42d4f4","#3cb44b","chocolate2","#4363d8","#bfef45","#911eb4","#f032e6","#a9a9a9"))+
+scale_fill_manual(values = c("salmon","gold2","#42d4f4","#3cb44b","chocolate2","#4363d8","#bfef45","#911eb4","#f032e6","#a9a9a9"))+
 theme(legend.direction = "vertical")+
-theme(legend.position = c(0.20, 0.92))+
+theme(legend.position = c(0.30, 0.85))+
 theme(legend.box = "horizontal")+
-theme(legend.title.align = 0)+ylim(c(0,60))+
+theme(legend.title.align = 0)+
 geom_text_repel(
 data = Sig,
 aes(label = Term),
@@ -81,8 +72,8 @@ col = "black")
 ## Write out the .png file
 ##-------------------------------------------------------------
 
-png("~/NP1vsOThers_Bubble_plot1.png",width = 12500, height = 7500, res = 600)
-p5
+png("~/Figure4e.png",width = 12500, height = 7500, res = 600)
+p4
 dev.off()
 
 
